93e37fbec6050d224e003b699194e0195b17e695
kirana salsa <kiranasalsapn@gmail.com>
2026-04-22 09:48:45 +0700

    new edit

--- edit.php
+++ edit.php
@@ -10,6 +10,9 @@ $d = mysqli_fetch_array($data);
     Nama: <input type="text" name="nama" value="<?= $d['nama']; ?>"><br>
     No Presensi: <input type="number" name="no_presensi" value="<?= $d['no_presensi']; ?>"><br>
     Kelas: <input type="text" name="kelas" value="<?= $d['kelas']; ?>"><br>
-    Foto: <input type="file" name="foto" value="<?= $d['foto']; ?>"><br>
+   Foto Saat Ini: <br>
+    <img src="uploads/<?= $d['foto']; ?>" width="100"><br>
+    Ganti Foto: <input type="file" name="foto"><br>
+    <small>*Kosongkan jika tidak ingin mengubah foto</small><br><br>
     <button type="submit" name="update">Update</button>
 </form>
