819d3fa78e8475df515338da2e007f43eb2a96da
kirana salsa <kiranasalsapn@gmail.com>
2026-05-06 07:24:14 +0700

    add css

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php include 'config/database.php'; ?>
+<!-- <?php include 'config/database.php'; ?>
 
 <h2>Daftar Siswa</h2>
 <a href="tambah.php">Tambah Siswa Baru</a>
@@ -27,4 +27,50 @@ while ($d = mysqli_fetch_array($data)) {
     </td>
 </tr>
 <?php } ?>
+</table> -->
+
+<?php include 'config/database.php'; ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Daftar Siswa</title>
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+
+<h2>Daftar Siswa</h2>
+
+<a href="tambah.php" class="btn-tambah">Tambah Siswa Baru</a>
+
+<table>
+<tr>
+    <th>Nama</th>
+    <th>No Presensi</th>
+    <th>Kelas</th>
+    <th>Foto</th>
+    <th>Aksi</th>
+</tr>
+
+<?php
+$data = mysqli_query($conn, "SELECT * FROM siswa");
+while ($d = mysqli_fetch_array($data)) {
+?>
+<tr>
+    <td><?= $d['nama']; ?></td>
+    <td><?= $d['no_presensi']; ?></td>
+    <td><?= $d['kelas']; ?></td>
+    <td>
+        <img src="assets/<?= $d['foto']; ?>" alt="foto">
+    </td>
+    <td class="aksi">
+        <a href="edit.php?id=<?= $d['id']; ?>" class="edit">Edit</a>
+        <a href="hapus.php?id=<?= $d['id']; ?>" class="hapus">Hapus</a>
+    </td>
+</tr>
+<?php } ?>
+
 </table>
+
+</body>
+</html>
